b892292b9d07089ec70b0f8f2fb508a53652b007
petermichuncc <plarrimo@uncc.edu>
2016-05-27 11:43:50 -0400

    first commit

---
+++ client/main.js
@@ -0,0 +1,28 @@
+//import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+ import uiRouter from 'angular-ui-router';
+angular.module('socially', [
+  angularMeteor,'ui.router'
+]);
+
+angular.module("socially").config(['$stateProvider', '$urlRouterProvider',
+    function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/");
+
+        $stateProvider
+            .state('home', {
+                url: "/home",
+                templateUrl: "client/home.html"
+            })
+            .state('state1', {
+                url: "/state1",
+               	templateUrl: "client/partials/state1.html"
+            })
+            
+    }
+]);
